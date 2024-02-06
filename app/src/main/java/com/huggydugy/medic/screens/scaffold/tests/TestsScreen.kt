eaeae7647a9d3f6958981b5ccc4286cc6e1b53f3
v0nd1 <vdovenko.dmitry@mail.ru>
2024-02-06 12:03:29 +0300

    Finish CardEvent LazyRow

--- app/src/main/java/com/huggydugy/medic/screens/scaffold/tests/TestsScreen.kt
+++ app/src/main/java/com/huggydugy/medic/screens/scaffold/tests/TestsScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -19,6 +20,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.paddingFrom
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -38,12 +40,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -52,6 +58,7 @@ import com.huggydugy.medic.ui.theme.BlueLight2
 import com.huggydugy.medic.ui.theme.BlueLight3
 import com.huggydugy.medic.ui.theme.BlueLight4
 import com.huggydugy.medic.ui.theme.Gray
+import com.huggydugy.medic.ui.theme.GrayLight
 import com.huggydugy.medic.ui.theme.GrayLight2
 import com.huggydugy.medic.ui.theme.GrayLight4
 import com.huggydugy.medic.ui.theme.Roboto
@@ -63,10 +70,10 @@ fun TestsScreen(navController: NavController){
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(horizontal = 10.dp)
+            .padding(horizontal = 15.dp, vertical = 15.dp)
     ) {
         ThemeSearchBar()
-        Spacer(modifier = Modifier.height(20.dp))
+        Spacer(modifier = Modifier.height(45.dp))
         Events()
 
     }
@@ -82,9 +89,10 @@ private fun Events(){
         text = "Акции и новости",
         color = Gray,
         fontFamily = Roboto,
-        fontSize = 25.sp
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 18.sp
     )
-    Spacer(modifier = Modifier.height(10.dp))
+    Spacer(modifier = Modifier.height(20.dp))
     LazyRow(
         modifier = Modifier
             .fillMaxWidth()
@@ -96,6 +104,7 @@ private fun Events(){
                 cards[it].descr,
                 cards[it].price,
                 cards[it].gradient,
+                cards[it].textWidth
             )
         }
 
@@ -108,65 +117,69 @@ private fun CardEvent(
     headText: String,
     descr: String,
     price: String,
-    gradient: List<Color>
+    gradient: List<Color>,
+    textWidth: Dp = 150.dp
 ){
 
     Card(
         modifier = Modifier
             .width(300.dp)
-            .height(200.dp),
+            .height(200.dp)
+            .padding(end = 15.dp),
     ) {
-        Box(
+        Row(
             modifier = Modifier
                 .fillMaxSize()
                 .background(
                     brush = Brush.horizontalGradient(
                         colors = gradient
                     )
-                )
-        ){
-            Row(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(start = 5.dp),
-                horizontalArrangement = Arrangement.SpaceBetween,
+                ),
+            horizontalArrangement = Arrangement.SpaceBetween,
 
             ) {
-                Column(
-                    modifier = Modifier.width(100.dp)
-                ) {
-                    Text(
-                        text = headText,
-                        textAlign = TextAlign.Left,
-                        fontSize = 20.sp,
-                        fontWeight = FontWeight.Bold,
-                        color = White
-                    )
+            Column(
+                modifier = Modifier
+                    .width(textWidth)
+                    .fillMaxHeight()
+                    .padding(start = 10.dp, top = 15.dp, bottom = 10.dp),
+                verticalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = headText,
+                    modifier = Modifier.wrapContentSize(),
+                    textAlign = TextAlign.Left,
+                    fontSize = 25.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = White
+                )
+                Column {
                     Text(
                         text = descr,
+                        modifier = Modifier.wrapContentSize(),
                         textAlign = TextAlign.Left,
+                        fontSize = 14.sp,
                         color = White,
                         fontFamily = Roboto
                     )
+                    Spacer(modifier = Modifier.height(5.dp))
                     Text(
                         text = price,
+                        modifier = Modifier.wrapContentSize(),
+                        fontSize = 25.sp,
                         textAlign = TextAlign.Left,
                         color = White,
+                        fontWeight = FontWeight.Bold,
                         fontFamily = Roboto
                     )
                 }
-
-
-
-                Image(
-                    painter = painterResource(id = image),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .fillMaxHeight()
-                )
-
             }
-
+            Image(
+                painter = painterResource(id = image),
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxSize()
+            )
         }
     }
 }
